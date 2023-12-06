07d3b0c2b4bee29e6f8f4cfffb0aaa3846af2e94
WI-Jeong <cpdls23@naver.com>
2023-12-06 12:27:06 +0900

    네트워크 리

---
+++ SACNetwork/Source/SACNetwork/SACNetworkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SACNetworkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SACNETWORK_API ASACNetworkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
--- Source/SAC1Network/SAC1Network.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SAC1Network.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SAC1Network, "SAC1Network" );
